6ee6bbcbf3469b32391fc084f60c86b34aae3e40
Alberto Punter <dragost11@gmail.com>
2013-03-12 19:24:00 +0100

    Subida de Archivos

---
+++ HTML.bat
@@ -0,0 +1,2 @@
+cd C:\dockbook\
+xsltproc C:\dockbook\docbook-xsl-1.78.0\html\docbook.xsl docbook.xml > docbook.html
---
+++ PDF.bat
@@ -0,0 +1,3 @@
+cd C:\dockbook\
+xsltproc C:\dockbook\docbook-xsl-1.78.0\fo\docbook.xsl docbook.xml > docbook.fo
+fop docbook.fo docbook.pdf
